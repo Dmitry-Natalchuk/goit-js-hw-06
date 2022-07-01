f33b7ed445a607c2b86b91242b14b2048316ca05
Dmitry Natalchuk <next1996@ukr.net>
2022-07-01 17:14:18 +0300

    add js1

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const totalCategori = document.querySelectorAll(".item");
+console.log(`Number of categories : ${totalCategori.length}`);
+
+const categoriesArray = [...totalCategori]
+  .map(categories => `Category: ${categories.children[0].textContent}
+    Elements: ${categories.children[1].children.length}`
+  ) .join("\n");
+console.log(categoriesArray);
