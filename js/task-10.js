c239d21b166e5afb9f965a1ca2d0c62a326109f5
Dmitry Natalchuk <next1996@ukr.net>
2022-07-09 17:18:48 +0300

    hw 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,15 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-//Постараюсь виконать пізніше,скіну вам на перевірку в лс
-//const divControls = document.querySelector("#controls")
-//const input = divControls.querySelector("input")
-//console.log(input)
-//console.log(divControls)
-//const btnCreate = divControls.querySelector("[data-create]")
-//const btnDestroy = divControls.querySelector("[data-destroy]")
-//const divBox = document.querySelector("#boxes") 
-//console.log(divBox)
+
+const [input, createBtn, destroyBtn] = document.querySelector('#controls').children
+const boxDiv = document.querySelector("#boxes")
 
 
 
+function onCreatrBtn () {
+  const newArray = []
+  for (let i = 1; i <= +input.value; i ++) {
+    const newBox = document.createElement("div");
+    newBox.style.height = newBox.style.width = `${30 + i * 10}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    newArray.push(newBox)
+  }
+  boxDiv.append(...newArray)
+}
+
+function ondestroyBtn () {
+  boxDiv.innerHTML = ""
+  input.value = ""
+}
+
+
+
+destroyBtn.addEventListener("click", ondestroyBtn)
+createBtn.addEventListener("click", onCreatrBtn)
