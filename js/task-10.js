8ecc1ecb17a2b04f1c5469f1625148d762d2f7f6
Dmitry Natalchuk <next1996@ukr.net>
2022-07-09 12:55:18 +0300

    test hw 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,3 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 //Постараюсь виконать пізніше,скіну вам на перевірку в лс
+//const divControls = document.querySelector("#controls")
+//const input = divControls.querySelector("input")
+//console.log(input)
+//console.log(divControls)
+//const btnCreate = divControls.querySelector("[data-create]")
+//const btnDestroy = divControls.querySelector("[data-destroy]")
+//const divBox = document.querySelector("#boxes") 
+//console.log(divBox)
+
+
+
