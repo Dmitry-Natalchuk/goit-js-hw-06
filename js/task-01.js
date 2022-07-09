6bdaa79373302cfd66e4e6e00df49612de19448e
Dmitry Natalchuk <next1996@ukr.net>
2022-07-09 15:03:32 +0300

    add

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ const totalCategori = document.querySelectorAll(".item");
 console.log(`Number of categories : ${totalCategori.length}`);
 
 const categoriesArray = [...totalCategori]
-  .map(categories => `Category: ${categories.children[0].textContent}
+.map(categories => `Category: ${categories.children[0].textContent}
     Elements: ${categories.children[1].children.length}`
-  ) .join("\n");
+  ).join("\n");
 console.log(categoriesArray);
